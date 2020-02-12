bbaa5467cc634d608d01ff7bd5b5e9657cd4467c
denish20003 <patel29denish@gmail.com>
2020-02-12 19:51:18 +0000

    Made change

--- nimbot.py
+++ nimbot.py
@@ -2,7 +2,6 @@ import os
 
 from discord.ext import commands
 from dotenv import load_dotenv
-
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
@@ -13,4 +12,6 @@ async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
 bot.run(token)
+
+# nim bot
 
