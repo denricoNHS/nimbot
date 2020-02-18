555905023eb67c996a91ca19704a69eb37b65fc7
laurengg <laurencoste77@gmail.com>
2020-02-18 19:51:01 +0000

    Add flip_coin function

--- nimbot.py
+++ nimbot.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -12,6 +12,9 @@ bot = commands.Bot(command_prefix='!')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
+@bot.command(name ="coin")
+async def flip_coin(ctx):
+    await ctx.send(random.choice(["Heads", "Tails"]))
+
 bot.run(token)
 
-#hello 
