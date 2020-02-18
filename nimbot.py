4d7c04ffc56ded6e538863ebf274d5ec02b39837
denish20003 <patel29denish@gmail.com>
2020-02-18 19:51:01 +0000

    Add flip_coin

--- nimbot.py
+++ nimbot.py
@@ -11,6 +11,10 @@ bot = commands.Bot(command_prefix='!')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
+@bot.command(name= 'flip_coin')
+async def flip_coin(ctx):
+    await ctx.send(random.choice(["Heads", "Tails"]))
+
 bot.run(token)
- # nim
+ 
  
