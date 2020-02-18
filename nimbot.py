a6e0376a8b375e319896c6ee00a214416a5d6aac
dymon-klyn <danyalternate@gmail.com>
2020-02-18 19:54:15 +0000

    dadad

--- nimbot.py
+++ nimbot.py
@@ -1,5 +1,4 @@
 import os
-import random 
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -12,10 +11,6 @@ bot = commands.Bot(command_prefix='!')
 @bot.command(name='nim')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
-
-@bot.command(name="flip_coin")
-async def flip_coin(ctx):
-    await ctx.send(random.choice(["heads", "tails"]))
 
 bot.run(token)
 
