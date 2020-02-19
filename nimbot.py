33b523740707f2ad2e6dac65510a764fbf222c19
sofisop <sofisop2001@gmail.com>
2020-02-19 15:58:35 +0000

    add average command to nimbot

--- nimbot.py
+++ nimbot.py
@@ -14,7 +14,12 @@ async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
 @bot.command(name='flip_coin')
-    async def flip_coin(ctx):
-        await ctx.send(random.choice(['Heads', 'Tails']))
+async def flip_coin(ctx):
+    await ctx.send(random.choice(['Heads', 'Tails']))
+
+@bot.command(name='average')
+async def average(ctx, a, b):
+    await ctx.send( (a+b)/2 )
+    
 
 bot.run(token)
