5c6cfeb67f2be94869f339283333c579e8c93eaf
Daniel Martinez <danyalternate@gmail.com>
2020-02-19 15:02:38 -0500

    added joke func

--- nimbot.py
+++ nimbot.py
@@ -1,7 +1,15 @@
 import os
+import request
 
 from discord.ext import commands
 from dotenv import load_dotenv
+
+def get_joke():
+    url = "https://icanhazdadjoke.com/"
+    header = {"Accept" : "application/json"}
+    joke = requests.get(url, headers = header).json()
+    return joke["joke"]
+
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -12,5 +20,8 @@ bot = commands.Bot(command_prefix='!')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
-bot.run(token)
+@bot.command(name="joke")
+async def tell_joke(ctx):
+    await ctx.send(get_joke())
 
+bot.run(token)
