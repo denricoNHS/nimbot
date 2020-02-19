95e2a0af6993e3b57c2366928ef33b232ad30a92
chavek <joshua.pvt1@gmail.com>
2020-02-19 19:54:25 +0000

    changes

--- nimbot.py
+++ nimbot.py
@@ -1,4 +1,4 @@
-'''import os
+import os
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -12,5 +12,9 @@ bot = commands.Bot(command_prefix='!')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
+@bot.command(name='avg')
+async def average(ctx, *args: float):
+    await ctx.send(sum(*args) / len(*args))
+
 bot.run(token)
-'''
+
