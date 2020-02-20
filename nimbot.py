15d38b6b491e96874e1d6d2a6b7b9ae9b80f1e52
daniel-sudo-max <danieladames13@icloud.com>
2020-02-20 17:12:46 +0000

    I added Enrico Compliments ;)

--- nimbot.py
+++ nimbot.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -11,6 +12,11 @@ bot = commands.Bot(command_prefix='!')
 @bot.command(name='nim')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
+
+
+@bot.command(name='Enricos_compliments')
+async def Enricos_compliments(ctx):           
+    await ctx.send(random.choice(["You need to do your eyebrows", "You know you suck right?","My hair is better than yours, and I am bald","You smell like hot Canadian Garbage","I have been to your house and I have concluded that you are fat.","Try not to create something that we have done millions and billions of times before please."]))
 
 bot.run(token)
 
