4276f994ff16f5149c90e24823f61796c6c0d9c1
chavek <joshua.pvt1@gmail.com>
2020-02-20 20:05:06 +0000

    Fibonacci  Sequence!

--- nimbot.py
+++ nimbot.py
@@ -4,6 +4,14 @@ import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
+def fib(n):
+    sign = -1 if n < 0 else 1
+    n = abs(n)
+    a, b = 0, 1
+    for _ in range(n):
+        yield sign * a
+        a, b = b, a + b
+
 def get_joke():
     url = "https://icanhazdadjoke.com/"
     header = {"Accept" : "application/json"}
@@ -22,6 +30,7 @@ async def help(ctx):
     flip_coin chooses which side of a coin you get,
     avg gets the average of any amount of numbers,
     joke spits out a bad dad joke please DO NOT SPAM.
+    fib gives you a sequence of fibonacci numbers
     ''')
 
 @bot.command(name='nim')
@@ -36,9 +45,13 @@ async def flip_coin(ctx):
 async def average(ctx, *args: float):
     await ctx.send(sum(*args) / len(*args))
 
-@bot.command(name="joke")
+@bot.command(name='joke')
 async def tell_joke(ctx):
     await ctx.send(get_joke())
+
+@bot.command(name='fib')
+async def fib(ctx, a: int):
+    await ctx.send(str(list(fibonacci(a))))
 
 bot.run(token)
 
