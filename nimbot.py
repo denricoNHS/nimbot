6aa75e21db9b9169fdfea3f4b8ac72a17b26cf05
sofisop <sofisop2001@gmail.com>
2020-02-20 16:57:40 +0000

    fortune cookies

--- nimbot.py
+++ nimbot.py
@@ -18,8 +18,13 @@ async def flip_coin(ctx):
     await ctx.send(random.choice(['Heads', 'Tails']))
 
 @bot.command(name='average')
-async def average(ctx, a, b):
-    await ctx.send( (a+b)/2 )
-    
+async def average(ctx, *args:float): #this is not working... maybe
+    await ctx.send(sum(args)/len(args) )
+
+@bot.command(name= 'FORTUNE_COOKIE')
+async def FORTUNE_COOKIE(ctx):
+    await ctx.send(random.choice(['your lucky numbers today: 10, 25, 3, 7', 'your lucky numbers today: 3, 4, 6, 7', 'a heart is an upside down butt', 'patience is key', 'the unlit candle scares no monckeys','you will buy new pants on tuesday','because you demand more from yourself, others respect you deeply', 'be careful or you could fall for some tricks today', 'go take a rest, you deserve it', 'if you continually give, you will continually have', 'it takes courage to admit fault', 'the end of something marks the start of something new', 'practice makes perfect... scratch that, practice makes better']))
+
+#I like that... so I'm gonna keep it
 
 bot.run(token)
