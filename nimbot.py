ade489ae50860292366232a40452fac497cb645f
chavek <joshua.pvt1@gmail.com>
2020-02-20 19:53:59 +0000

    Helpfile and dad jokes

--- nimbot.py
+++ nimbot.py
@@ -4,10 +4,25 @@ import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
+def get_joke():
+    url = "https://icanhazdadjoke.com/"
+    header = {"Accept" : "application/json"}
+    joke = requests.get(url, headers = header).json()
+    return joke["joke"]
+
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='help')
+async def help(ctx):
+    await ctx.send('''Commands: nim, flip_coin, avg, joke
+    nim plays a game of nim,
+    flip_coin chooses which side of a coin you get,
+    avg gets the average of any amount of numbers,
+    joke spits out a bad dad joke please DO NOT SPAM.
+    ''')
 
 @bot.command(name='nim')
 async def play_nim(ctx):
@@ -20,6 +35,10 @@ async def flip_coin(ctx):
 @bot.command(name='avg')
 async def average(ctx, *args: float):
     await ctx.send(sum(*args) / len(*args))
+
+@bot.command(name="joke")
+async def tell_joke(ctx):
+    await ctx.send(get_joke())
 
 bot.run(token)
 
