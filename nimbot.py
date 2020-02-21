670cfbad3d0582cc2cd9af8bd079e063ccb364e7
chavek <joshua.pvt1@gmail.com>
2020-02-21 21:45:48 +0000

    fibonacci function condensed and improved as well as its description

--- nimbot.py
+++ nimbot.py
@@ -11,7 +11,7 @@ token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='nim', help="Should play a game of nim")
+@bot.command(name='nim', help="Should play a game of nim.")
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
@@ -20,11 +20,11 @@ async def play_nim(ctx):
 async def flip_coin(ctx):
     await ctx.send(random.choice(["Heads", "Tails"]))
 
-@bot.command(name="avg", help="Gets the average of multiple numbers")
+@bot.command(name="avg", help="Gets average of multiple numbers.")
 async def avg(ctx, *args: float):
     await ctx.send(sum(args) / len(args))
 
-@bot.command(name="joke", help="bad dad jokes")
+@bot.command(name="joke", help="bad dad jokes.")
 
 async def joke(ctx):
     async with aiohttp.ClientSession(headers={"Accept":"application/json"}) as session:
@@ -32,24 +32,19 @@ async def joke(ctx):
             data = await resp.json()
             await ctx.send(data["joke"])
 
-@bot.command(name="d6", help="Rolls a 6-sided die")
+@bot.command(name="d6", help="Rolls a 6-sided die.")
 async def d6(ctx):
     await ctx.send(random.randint(1, 7))
 
 
-@bot.command(name="fib", help="Creates a sequence of fibonacci numbers")
+@bot.command(name="fib", help="Creates a defined number of the fibonacci sequence")
 async def fibonacci(ctx, n: int):
-    fib = []
-    if n > 0:
-        a, b = 0, 1
-        for _ in range(n):
-            fibonachos.append(a)
-            a, b = b, a + b
-    elif n < 0:
-        a, b = 0, -1
-        for _ in range(abs(n)): # list(range()) does not work with negatives
-            fibonachos.append(a)
-            a, b = b, a - b
-    await ctx.send(fibonachos)
+    fibonachos = []
+    sign = 1 if n > 0 else -1
+    a, b = 0, sign * 1
+    for _ in range(abs(n)):
+        fibonachos.append(a)
+        a, b = b, a + sign * b
+    await ctx.send(str(fibonachos)[1:-1]) # converts to string to remove brackets
 
 bot.run(token)
