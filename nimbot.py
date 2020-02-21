85668393a575fd52d116baec0517cd27e166b3c7
amy1030 <azogue.amy@gmail.com>
2020-02-21 00:18:48 +0000

    This bot will only say no when you ask for help

--- nimbot.py
+++ nimbot.py
@@ -16,12 +16,18 @@ async def play_nim(ctx):
 @bot.command(name="flip-coin")
 async def flip_coin(ctx):
     await ctx.send(random.choice(["Heads", "Tails"]))
+
 @bot.command(name= 'average')
-async def average(ctx, a, b):
-    await ctx.send((a+b)/2)
+async def average(ctx, *args: float):
+    await ctx.send(sum(args)/len(args))
+
+@bot.command(name= 'help')
+async def help(ctx):
+    await ctx.send(random.choice(["Nah I think you git this 😉", "Help yourself 😠", "I would rather not 😒 " , "YOU ALWAYS ASK FOR HELP BUT NO ONE EVER ASKS ME IF I NEED HELP 😭" ]))
 
 
 bot.run(token)
 
 #Amy Azogue
+
 
