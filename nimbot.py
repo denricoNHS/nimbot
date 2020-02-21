ba2474d25f331d47966eff91c5b9d85a0a2aabcb
tatiana110504 <tatiana110504@gmail.com>
2020-02-21 03:36:01 +0000

    inspirational

--- nimbot.py
+++ nimbot.py
@@ -14,7 +14,7 @@ async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
 @bot.command(name='flip_coin')
-async def flip_coin():
+async def flip_coin(ctx):
     await ctx.send(random.choice(["Heads", "Tails"]))
 
 @bot.command(name= 'average')
@@ -22,9 +22,8 @@ async def average(ctx, a, b):
     await ctx.send(a+b/2)
     
 @bot.command(name= 'inspirational_quotes')
-async def inspirational_quotes():
-    await ctx.send(random.choice(["Your limitation—it's only your imagination.", "Push yourself, because no one else is going to do it for you."]))
-
+async def inspirational_quotes(ctx):
+    await ctx.send(random.choice(["Genius is one percent inspiration and ninety-nine percent perspiration.","You can observe a lot just by watching.","A house divided against itself cannot stand.","Difficulties increase the nearer we get to the goal.", "Fate is in your hands and no one elses","Be the chief but never the lord.","Nothing happens unless first we dream.","Well begun is half done.","Life is a learning experience, only if you learn.","Self-complacency is fatal to progress.","Peace comes from within. Do not seek it without.","What you give is what you get.","We can only learn to love by loving.","Life is change. Growth is optional. Choose wisely."]))
 bot.run(token)
 #Tatiana Nunez
 
