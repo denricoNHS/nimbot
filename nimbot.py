77321a3a242dcbc3e9d61e176a6cab76ca25b925
sofisop <sofisop2001@gmail.com>
2020-02-21 01:50:23 +0000

    (should be) fixed nimbot copy. with joke and fixed avg

--- nimbot.py
+++ nimbot.py
@@ -1,8 +1,16 @@
 import os
 import random
+import requests
 
 from discord.ext import commands
 from dotenv import load_dotenv
+
+def get_joke():
+    url = "https://icanhazdadjoke.com/"
+    header = {"Accept" : "application/json"}
+    joke = requests.get(url, headers = header).json()
+    return joke["joke"]
+
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -17,15 +25,17 @@ async def play_nim(ctx):
 async def flip_coin(ctx):
     await ctx.send(random.choice(['Heads', 'Tails']))
 
-@bot.command(name='average')
-async def average(ctx, *args:float): #this is not working... maybe
-    await ctx.send(sum(args)/len(args) )
+@bot.command(name='avg')
+async def avg(ctx, *args: float):
+    await ctx.send(sum(args) / len(args))
+
+@bot.command(name="joke")
+async def tell_joke(ctx):
+    await ctx.send(get_joke())
 
 @bot.command(name='FORTUNE_COOKIE')
 async def FORTUNE_COOKIE(ctx):
     lst = ['an unlit candle frightens no monckeys','just wait for the right moment. keep your eyes and ears peeled','romance could divert your attention away from money matters today','a good book is the best of friends, the same today and forever', 'dwelling on the negative simply contributes to its power', 'just wait for the right moment... attentively', 'trust your decisions']
     await ctx.send(random.choice)
-
-#I like that... so I'm gonna keep it
 
 bot.run(token)
