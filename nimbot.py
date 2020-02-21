968e99bd22244ce103c862245cb004d7e03b81a1
2salsa2 <sksky217@gmail.com>
2020-02-21 01:59:37 +0000

    My Bot

--- nimbot.py
+++ nimbot.py
@@ -21,11 +21,18 @@ async def flip_coin(ctx):
 async def average(ctx, a, b):
     await ctx.send((a+b)/2)
 
-average(2, 3)
+@bot.command(name="Odds")
+async def odds(ctx):
+    await random.randint(1,10)
 
-
-
-
+@bot.command(name="d")
+async  def alpha(str):
+    str_1 = ("Hi, my name is Rosco! Stop doing what I do!")
+    words = str_1.split()
+    words.sort()
+    print("they are in alpha order!")
+    for word in words:
+        print(word)
 
 
 bot.run(token)
