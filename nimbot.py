5677cf2fcedf30466fef994304912c06c44da675
2salsa2 <sksky217@gmail.com>
2020-02-22 02:35:14 +0000

    Discord ALPHA FINAL ENRICO

--- nimbot.py
+++ nimbot.py
@@ -25,13 +25,13 @@ async def average(ctx, a, b):
 async def odds(ctx):
     await random.randint(1,10)
 
-@bot.command(name="d")
+@bot.command(name="Alpha")
 async  def alpha(ctx, str):
     str_1 = ("Hi, my name is Rosco! Stop doing what I do!")
     words = str_1.split()
     words.sort()
     print("they are in alpha order!")
-await for word in words:
+    for word in words:
         print(word)
     
 
