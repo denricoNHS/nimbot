82fa94f15bfdf889bfc09de655684f10ef077db1
angie180323 <angie13183@gmail.com>
2020-02-22 05:10:49 +0000

    babyname

--- nimbot.py
+++ nimbot.py
@@ -16,3 +16,13 @@ async def play_nim(ctx):
 async def flip_coin():
     await ctx.send(random.choice(["heads","tails"]))
 bot.run(token)
+
+import random
+@bot.command(name="Baby_Name",help="Write girl and find out girl names and write boy to do the same for boys")
+async def baby_names(n):
+    girlnames = ("Mia","Dinna","Pam","Anny","Jam","Lena","Karla","Ally","Alex","Kate","Bonnie","Carmen","Emma","Alicia","GG","Ginna","Angie","Jennifer","Ashley","May","Kimberly","Amy","Emily","Halsey","Selena","Demi","Diana","Valerie","Sofia","Robin","July","Lexi")
+    boynames = ("Zack","Cody","Mo","Luis","James","Mike","Liam","Lex","Kobe","Willy","Justin","Nick","Adrian","Leo","David","Andy","Luke","Richard","DJ","Steve","Ken","Mat","Asher","Jack","Sebastian","Oliver","Edward","Mick","Chris","Brandon","CJ","Henry")
+    if n == 'girl':
+        return (random.choice(girlnames))
+    else: 
+        return (random.choice(boynames))
