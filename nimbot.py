f9607030a996b90d63511cd48dfaae6a709e22a5
Daniel Martinez <danyalternate@gmail.com>
2020-02-21 22:08:38 -0500

    fixed formating

--- nimbot.py
+++ nimbot.py
@@ -11,24 +11,29 @@ token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
+
 @bot.command(name='nim')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
+
 
 @bot.command(name='flip_coin')
 async def flip_coin(ctx):
     await ctx.send(random.choice(["Heads", "Tails"]))
 
+
 @bot.command(name="avg")
 async def avg(ctx, *args: float):
     await ctx.send(sum(args) / len(args))
 
+
 @bot.command(name="joke")
 async def joke(ctx):
-    async with aiohttp.ClientSession(headers={"Accept":"application/json"}) as session:
+    async with aiohttp.ClientSession(headers={"Accept": "application/json"}) as session:
         async with session.get("https://icanhazdadjoke.com/") as resp:
             data = await resp.json()
             await ctx.send(data["joke"])
+
 
 @bot.command(name="game")
 async def info(ctx, *args: str):
