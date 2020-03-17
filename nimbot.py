652b9c0c863616fb8669e1d7046d5fef57f27de5
Denish Patel <patel29denish@gmail.com>
2020-03-17 15:04:04 -0400

    code

--- nimbot.py
+++ nimbot.py
@@ -9,20 +9,10 @@ token = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='nim')
-async def play_nim(ctx):
-    await ctx.send("Still working on it, kiddo.")
-
-@bot.command(name= 'flip_coin')
-async def flip_coin(ctx):
-    await ctx.send(random.choice(["Heads", "Tails"]))
-
-@bot.command(name= 'avg')
-async def avg(ctx,*args : float):
-    await ctx.send(sum(args)/ len(args))
-
-@bot.command(name= "day of the week")
-async def day_of_the_week(ctx,y,m,d):
-    await ctx.send(datetime.datetime(y,m,d).strftime("%A"))
+async def casino(ctx):
+    print ("1.Coin Flip")
+    print ("2.ROll the Dice")
+    await("Enter which number game would you like to play")
 
 bot.run(token)
 
